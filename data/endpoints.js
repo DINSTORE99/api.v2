6e4a4146d3818add68f0800b77893d111e14599a
DINSTORE99 <dinstoree41@gmail.com>
2026-07-22 00:25:30 +0700

    Create endpoints.js

---
+++ data/endpoints.js
@@ -0,0 +1,24 @@
+const ENDPOINTS = [
+
+{
+name: "Brat",
+path: "/m/brat",
+method: "GET",
+params: [
+"text",
+"delay"
+]
+},
+
+{
+name: "Brat Animated",
+path: "/m/brat",
+method: "GET",
+params: [
+"text",
+"isAnimated",
+"delay"
+]
+}
+
+];
