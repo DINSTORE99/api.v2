618d8e86309802cf37a26e8cb100f4f1e86431b9
DINSTORE99 <dinstoree41@gmail.com>
2026-07-22 00:27:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-ENDPOINTS.forEach(api => {
+const BASE_URL="/api/siput";
 
-// otomatis bikin card
+function executeRequest(){
 
-});
+const endpoint=document.getElementById("endpoint").value;
+const text=document.getElementById("text").value;
+const delay=document.getElementById("delay").value;
+
+document.getElementById("result").innerHTML=`
+<img src="${BASE_URL}${endpoint}?text=${encodeURIComponent(text)}&delay=${delay}" width="100%">
+`;
+
+}
