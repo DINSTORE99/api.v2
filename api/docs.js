ce3156711276aba5c8544e0242e381cfc6b73be9
DINSTORE99 <dinstoree41@gmail.com>
2026-08-11 23:43:08 +0700

    Update docs.js

--- api/docs.js
+++ api/docs.js
@@ -1,90 +1,28 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    creator: "DINSTORE",
-    name: "DINSTORE API",
-    version: "1.0.0",
-
-    categories: [
-      {
-        name: "Downloader",
-        icon: "download",
-
-        endpoints: [
-          {
-            name: "TikTok Downloader",
-            method: "GET",
-            path: "/api/tiktok",
-
-            description: "Download video TikTok tanpa watermark.",
-
-            parameters: [
-              {
-                name: "url",
-                type: "string",
-                required: true,
-                description: "URL TikTok"
-              }
-            ],
-
-            example:
-              "/api/tiktok?url=https://vt.tiktok.com/ZS4c5fT85/"
-          },
-
-          {
-            name: "Instagram Downloader",
-            method: "GET",
-            path: "/api/instagram",
-
-            description: "Download video Instagram.",
-
-            parameters: [
-              {
-                name: "url",
-                type: "string",
-                required: true,
-                description: "URL Instagram"
-              }
-            ],
-
-            example:
-              "/api/instagram?url=https://www.instagram.com/reel/..."
-          }
-        ]
-      },
-
-      {
-        name: "Tools",
-        icon: "tools",
-
-        endpoints: [
-          {
-            name: "QRIS Generator",
-            method: "GET",
-            path: "/api/tools/qrisgen",
-
-            description: "Generate QRIS dengan nominal tertentu.",
-
-            parameters: [
-              {
-                name: "nominal",
-                type: "number",
-                required: true,
-                description: "Nominal pembayaran"
-              },
-              {
-                name: "url",
-                type: "string",
-                required: true,
-                description: "URL QRIS"
-              }
-            ],
-
-            example:
-              "/api/tools/qrisgen?nominal=10000&url=https://..."
-          }
-        ]
-      }
-    ]
-  });
+{
+  "success": true,
+  "creator": "DINSTORE",
+  "name": "DINSTORE API",
+  "version": "1.0.0",
+  "categories": [
+    {
+      "name": "Downloader",
+      "icon": "download",
+      "endpoints": [
+        {
+          "name": "TikTok Downloader",
+          "method": "GET",
+          "path": "/api/tiktok",
+          "description": "Download video TikTok tanpa watermark.",
+          "parameters": [
+            {
+              "name": "url",
+              "type": "string",
+              "required": true,
+              "description": "URL TikTok"
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
